0ff678584eb053945f6f15516de90a204a11ad31
Ni-a1 <nityasingla1@gmail.com>
2026-04-30 23:43:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv(r"C:\Users\LENOVO\AppData\Local\Programs\Python\Python311\pm25_results.csv")
+time = data["t_plot"]
+actual = data["Y"]
+predicted = data["Y_pred"]
+st.title("PM2.5 Dashboard (From MATLAB Output)")
+mse = ((actual - predicted) ** 2).mean()
+st.metric("MSE", round(mse, 2))
+fig, ax = plt.subplots()
+ax.plot(time, actual, label="Actual (MATLAB)")
+ax.plot(time, predicted, label="Predicted (MATLAB)")
+ax.set_title("PM2.5 Comparison")
+ax.set_xlabel("Time")
+ax.set_ylabel("PM2.5")
+ax.legend()
+ax.grid(True)
+st.pyplot(fig)
+
