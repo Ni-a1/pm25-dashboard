b517da5fe9e20fbfb386eb513c1ea34b0aa933ff
Ni-a1 <nityasingla1@gmail.com>
2026-05-01 12:01:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,21 @@ predicted = data["Y_pred"]
 st.title("PM2.5 Dashboard (From MATLAB Output)")
 mse = ((actual - predicted) ** 2).mean()
 st.metric("MSE", round(mse, 2))
+view_option = st.selectbox(
+    "Select View",
+    ["Actual vs Predicted", "Only Actual", "Only Predicted"]
 fig, ax = plt.subplots()
-ax.plot(time, actual, label="Actual (MATLAB)")
-ax.plot(time, predicted, label="Predicted (MATLAB)")
+
+if view_option == "Actual vs Predicted":
+    ax.plot(time, actual, label="Actual (MATLAB)")
+    ax.plot(time, predicted, label="Predicted (MATLAB)")
+
+elif view_option == "Only Actual":
+    ax.plot(time, actual, label="Actual (MATLAB)")
+
+else:
+    ax.plot(time, predicted, label="Predicted (MATLAB)")
+    
 ax.set_title("PM2.5 Comparison")
 ax.set_xlabel("Time")
 ax.set_ylabel("PM2.5")
