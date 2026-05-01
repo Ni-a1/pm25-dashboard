4d33d8f9ffab63906808d3898a0b2d38c8c6b46e
Ni-a1 <nityasingla1@gmail.com>
2026-05-01 12:05:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ time = pd.to_datetime(data["t_plot"])
 actual = data["Y"]
 predicted = data["Y_pred"]
 st.title("PM2.5 Dashboard (From MATLAB Output)")
-mse = ((actual - predicted) ** 2).mean()
-st.metric("MSE", round(mse, 2))
+
 view_option = st.selectbox(
     "Select View",
     ["Actual vs Predicted", "Only Actual", "Only Predicted"])
@@ -31,5 +30,21 @@ ax.set_ylabel("PM2.5")
 ax.legend()
 ax.grid(True)
 st.pyplot(fig, clear_figure=True)
+mse = ((actual - predicted) ** 2).mean()
+st.metric("MSE", round(mse, 2))
+rmse = mse ** 0.5
+st.metric("RMSE", round(rmse, 2))
+
+error = actual - predicted
+
+fig2, ax2 = plt.subplots()
+ax2.plot(time, error)
+ax2.set_title("Prediction Error Over Time")
+ax2.set_xlabel("Time")
+ax2.set_ylabel("Error")
+ax2.grid(True)
+
+st.pyplot(fig2)
 if st.checkbox("Show Raw Data"):
     st.write(data)
+    
